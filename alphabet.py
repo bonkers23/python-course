fcf8c62f70b50547a546a365cd2836f832a16783
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:06:50 +0530

    M1L6 ACP: Checking alphabets

---
+++ alphabet.py
@@ -0,0 +1,12 @@
+# M1L6 ACP
+# Topic: Checking Alphabets
+# This program checks whether a character is an alphabet or not
+
+character = "A"
+
+if character.isalpha():
+    print(character, "is an Alphabet")
+else:
+    print(character, "is not an Alphabet")
+
+# End of program
