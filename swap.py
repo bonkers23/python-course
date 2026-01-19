af66869247ba3c98725144f853030d98906393b0
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:19:41 +0530

    M1L8 ACP: Number swap

---
+++ swap.py
@@ -0,0 +1,21 @@
+# M1L8 ACP
+# Topic: Number Swap
+# This program swaps two numbers
+
+a = 10
+b = 20
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping using a temporary variable
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+
+# End of program
