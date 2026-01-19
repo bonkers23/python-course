d94edc4e16fbff1424b495c3f3986bf83c5aaac9
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:09:39 +0530

    M1L7 Activity 2: Bitwise operator

---
+++ bitwise.py
@@ -0,0 +1,12 @@
+# M1L7 Activity 2
+# Topic: Bitwise Operator
+# This program demonstrates basic bitwise operators
+
+a = 5   # 0101
+b = 3   # 0011
+
+print("a & b:", a & b)   # AND
+print("a | b:", a | b)   # OR
+print("a ^ b:", a ^ b)   # XOR
+
+# End of program
