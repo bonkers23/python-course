9a9dbe5a62870d240695a1709cde9b7488b44fa3
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:04:12 +0530

    M1L6 Activity 2: Not equal operator

---
+++ notEqual.py
@@ -0,0 +1,13 @@
+# M1L6 Activity 2
+# Topic: NOT Equal Operator
+# This program demonstrates the use of the not equal (!=) operator
+
+password = "python123"
+entered_password = "python321"
+
+if entered_password != password:
+    print("Incorrect password")
+else:
+    print("Password matched")
+
+# End of program
