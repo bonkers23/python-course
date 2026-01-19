07b148e3a6affb254821aff6e59de9e4ca43645e
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:26:25 +0530

    first commit

---
+++ comments.py
@@ -0,0 +1,15 @@
+# M1L1 Activity 1
+# Topic: Print Statements and Comments
+# This program demonstrates the use of print statements and comments in Python
+
+# This is a single-line comment
+print("Hello, Python!")
+
+# Printing multiple messages
+print("Welcome to the Coding Class")
+print("Learning Python is fun!")
+
+# This print statement displays a sentence
+print("Comments are used to explain code and make it easier to understand")
+
+# End of program
