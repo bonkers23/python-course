e289dfdd18db3fc572d46f9552716638fd216713
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:02:49 +0530

    M1L6 Activity 1: AND OR operator

---
+++ operator.py
@@ -0,0 +1,22 @@
+# M1L6 Activity 1
+# Topic: AND-OR Operator
+# This program demonstrates the use of AND and OR operators
+
+age = 15
+has_id_card = True
+
+# Using AND operator
+if age >= 13 and has_id_card:
+    print("Access granted using AND operator")
+else:
+    print("Access denied using AND operator")
+
+# Using OR operator
+is_weekend = False
+
+if age < 18 or is_weekend:
+    print("Special discount available using OR operator")
+else:
+    print("No discount available")
+
+# End of program
