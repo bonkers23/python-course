df2137caa270f6ba0e47924e076f7dc75b4933ab
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:46:37 +0530

    M1L4 Activity 1: Average

---
+++ average.py
@@ -0,0 +1,15 @@
+# M1L4 Activity 1
+# Topic: Average
+# This program calculates the average of three numbers
+
+num1 = 10
+num2 = 20
+num3 = 30
+
+total = num1 + num2 + num3
+average = total / 3
+
+print("Numbers are:", num1, num2, num3)
+print("Average is:", average)
+
+# End of program
