5578b2879966390e094504b37fe3cc663b7edb8c
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:45:04 +0530

    M1L3 ACP: Congratulations message

---
+++ conrats.py
@@ -0,0 +1,14 @@
+# M1L3 ACP
+# Topic: Congratulations Message
+# This program prints a congratulatory message using strings
+
+name = "Student"
+score = 95
+
+message = "Congratulations " + name + "!"
+
+print(message)
+print("You have scored", score, "marks.")
+print("Keep up the good work!")
+
+# End of program
