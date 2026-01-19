964adcb119c51a724b2bb6db9274209d321b2020
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:55:36 +0530

    M1L5 Activity 3: Checking number greater or smaller

---
+++ checkingNo.py
@@ -0,0 +1,15 @@
+# M1L5 Activity 3
+# Topic: Checking the number greater or smaller
+# This program compares two numbers
+
+num1 = 25
+num2 = 40
+
+if num1 > num2:
+    print(num1, "is greater than", num2)
+elif num1 < num2:
+    print(num1, "is smaller than", num2)
+else:
+    print("Both numbers are equal")
+
+# End of program
