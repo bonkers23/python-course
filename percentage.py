dc43ffa1be020f68ca2e62761782ab82c1b3b61f
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:48:52 +0530

    M1L4 Activity 3: Percentage calculation

---
+++ percentage.py
@@ -0,0 +1,14 @@
+# M1L4 Activity 3
+# Topic: Percentage Calculation
+# This program calculates the percentage of marks
+
+total_marks = 500
+obtained_marks = 420
+
+percentage = (obtained_marks / total_marks) * 100
+
+print("Total Marks:", total_marks)
+print("Obtained Marks:", obtained_marks)
+print("Percentage:", percentage, "%")
+
+# End of program
