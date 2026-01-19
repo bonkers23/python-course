954feaee127ffdeae97ef18249faaece20b99987
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:08:03 +0530

    M1L7 Activity 1: Identity operator

---
+++ identity.py
@@ -0,0 +1,13 @@
+# M1L7 Activity 1
+# Topic: Identity Operator
+# This program demonstrates the use of identity operators (is / is not)
+
+a = 10
+b = 10
+c = 20
+
+print("a is b:", a is b)
+print("a is c:", a is c)
+print("a is not c:", a is not c)
+
+# End of program
