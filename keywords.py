c58857b8e63be8bc7f1729f9965ed24b681cdb82
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:38:53 +0530

    M1L2 Activity 3: Keywords

---
+++ keywords.py
@@ -0,0 +1,19 @@
+# M1L2 Activity 3
+# Topic: Keywords
+# This program explains Python keywords with examples
+
+# 'True' and 'False' are keywords
+is_active = True
+is_logged_in = False
+
+# 'None' keyword
+value = None
+
+print("Is Active:", is_active)
+print("Is Logged In:", is_logged_in)
+print("Value:", value)
+
+# Keywords like 'print' and 'import' are reserved words
+print("Keywords are reserved words in Python")
+
+# End of program
