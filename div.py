4ac74f7a4ccd17a65122b69d5735a7164207de77
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:15:40 +0530

    M1L8 Activity 2: Divisible number

---
+++ div.py
@@ -0,0 +1,12 @@
+# M1L8 Activity 2
+# Topic: Divisible Number
+# This program checks whether a number is divisible by 5 and 11
+
+number = 55
+
+if number % 5 == 0 and number % 11 == 0:
+    print(number, "is divisible by both 5 and 11")
+else:
+    print(number, "is not divisible by both 5 and 11")
+
+# End of program
