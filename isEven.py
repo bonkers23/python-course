c6244e3d4c64c201ff5f969f7cc95f21d9089178
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:35:37 +0530

    M1L1 ACP: Odd even flowchart

---
+++ isEven.py
@@ -0,0 +1,14 @@
+# M1L1 ACP
+# Topic: Odd Even Flowchart
+# This program represents the flowchart to check whether a number is odd or even
+
+print("Flowchart: Check Odd or Even Number")
+
+print("Start")
+print("Input a number")
+print("Is the number divisible by 2?")
+print("If YES → Print 'Even Number'")
+print("If NO → Print 'Odd Number'")
+print("End")
+
+# End of flowchart
