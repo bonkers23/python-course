397eada659ac87ff4010ed2852336a84edb1f10b
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:18:17 +0530

    M1L8 Activity 4: Average speed

---
+++ speed.py
@@ -0,0 +1,14 @@
+# M1L8 Activity 4
+# Topic: Average Speed
+# This program calculates the average speed
+
+distance = 150   # in kilometers
+time = 3         # in hours
+
+average_speed = distance / time
+
+print("Distance:", distance, "km")
+print("Time:", time, "hours")
+print("Average Speed:", average_speed, "km/h")
+
+# End of program
