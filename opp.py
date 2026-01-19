e41b41fd53b3276a633e5ca9f06691285d3047cf
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:14:25 +0530

    M1L8 Activity 1: Operator precedence

---
+++ opp.py
@@ -0,0 +1,11 @@
+# M1L8 Activity 1
+# Topic: Operator Precedence
+# This program demonstrates operator precedence in Python
+
+result1 = 10 + 5 * 2
+result2 = (10 + 5) * 2
+
+print("Result without brackets:", result1)
+print("Result with brackets:", result2)
+
+# End of program
