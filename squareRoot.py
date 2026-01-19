b876ad6d17b6e893b5effbc45df1e08d4db14ab4
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:50:20 +0530

    M1L4 ACP: Square root

---
+++ squareRoot.py
@@ -0,0 +1,12 @@
+# M1L4 ACP
+# Topic: Square Root
+# This program calculates the square root of a number
+
+number = 25
+
+square_root = number ** 0.5
+
+print("Number:", number)
+print("Square Root:", square_root)
+
+# End of program
