84177c1d3394c08dc3206f8d93187f4421440084
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:34:20 +0530

    M1L1 Activity 4: Platform introduction legacy activity

---
+++ playform.py
@@ -0,0 +1,16 @@
+# M1L1 Activity 4
+# Topic: Platform Introduction (Legacy Activity)
+# This program introduces the coding platform to students
+
+print("Welcome to the Coding Platform!")
+
+print("This platform helps students learn programming step by step.")
+print("You can write, run, and test your code here.")
+print("You will learn Python through fun activities and projects.")
+
+print("Always follow the instructions given in each lesson.")
+print("Practice regularly to improve your coding skills.")
+
+print("Happy Coding!")
+
+# End of platform introduction
