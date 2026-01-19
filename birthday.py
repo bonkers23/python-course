3a6ec448194d270e998fbaf8b2d134cf02b033fd
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:39:55 +0530

    M1L2 ACP: Storing birthdays

---
+++ birthday.py
@@ -0,0 +1,14 @@
+# M1L2 ACP
+# Topic: Storing Birthdays
+# This program stores birthdays using variables
+
+name1 = "Rahul"
+birthday1 = "12 March"
+
+name2 = "Ananya"
+birthday2 = "25 July"
+
+print(name1, "'s birthday is on", birthday1)
+print(name2, "'s birthday is on", birthday2)
+
+# End of program
