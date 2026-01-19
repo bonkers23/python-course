7e80996622dd4f007d1036ff7ce9268c83dbf412
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:56:53 +0530

    M1L5 Activity 4: Odd even

---
+++ isOdd.py
@@ -0,0 +1,12 @@
+# M1L5 Activity 4
+# Topic: Odd-Even
+# This program checks whether a number is odd or even
+
+number = 18
+
+if number % 2 == 0:
+    print(number, "is an Even number")
+else:
+    print(number, "is an Odd number")
+
+# End of program
