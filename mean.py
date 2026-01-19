60940a1f24c50e1c6f14d81404ec89ec51196bfb
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:17:01 +0530

    M1L8 Activity 3: Mean value

---
+++ mean.py
@@ -0,0 +1,14 @@
+# M1L8 Activity 3
+# Topic: Mean Value
+# This program calculates the mean value of numbers
+
+num1 = 12
+num2 = 18
+num3 = 24
+
+mean = (num1 + num2 + num3) / 3
+
+print("Numbers are:", num1, num2, num3)
+print("Mean Value:", mean)
+
+# End of program
