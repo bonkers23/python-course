6726e812de850c3ff73a0f96ff2dc129cf5afc2a
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:37:47 +0530

    M1L2 Activity 2: Understanding the variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# M1L2 Activity 2
+# Topic: Understanding the Variables
+# This program explains the use of variables in Python
+
+name = "Student"
+age = 12
+grade = 7
+is_coding_fun = True
+
+print("Name:", name)
+print("Age:", age)
+print("Grade:", grade)
+print("Is coding fun?", is_coding_fun)
+
+# End of program
