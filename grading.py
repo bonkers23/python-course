40273435334e5be528336118d60f9d0c71dcb0d5
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:10:59 +0530

    M1L7 Activity 3: Grading system

---
+++ grading.py
@@ -0,0 +1,18 @@
+# M1L7 Activity 3
+# Topic: Grading System
+# This program assigns grades based on marks
+
+marks = 82
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+elif marks >= 40:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+# End of program
