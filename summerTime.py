b185b37bbf6feefe54a7d0b855f4a488bdfdc97d
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:57:51 +0530

    M1L5 ACP: Summer times

---
+++ summerTime.py
@@ -0,0 +1,12 @@
+# M1L5 ACP
+# Topic: Summer Times
+# This program checks if it is summer time based on the month number
+
+month = 5  # 4 = April, 5 = May, 6 = June
+
+if month == 4 or month == 5 or month == 6:
+    print("It is Summer Time")
+else:
+    print("It is not Summer Time")
+
+# End of program
