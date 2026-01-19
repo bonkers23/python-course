cdeabc264ea2487967fa8904d796844d183a134c
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 01:12:39 +0530

    M1L7 ACP: ASCII value

---
+++ ascii.py
@@ -0,0 +1,12 @@
+# M1L7 ACP
+# Topic: ASCII Value
+# This program finds the ASCII value of a character
+
+character = 'A'
+
+ascii_value = ord(character)
+
+print("Character:", character)
+print("ASCII Value:", ascii_value)
+
+# End of program
