786375f0ccaa8453f2754f6b619b119dab0b7458
Shariq Hashmi <shariq.xiiv@gmail.com>
2026-01-20 00:51:54 +0530

    M1L5 Activity 1: Conditional statements

---
+++ statements.py
@@ -0,0 +1,12 @@
+# M1L5 Activity 1
+# Topic: Conditional Statements
+# This program demonstrates the use of if-else statements
+
+number = 10
+
+if number > 0:
+    print("The number is positive")
+else:
+    print("The number is not positive")
+
+# End of program
